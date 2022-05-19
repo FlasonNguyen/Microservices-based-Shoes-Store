0b675d8223a23976e2a42f055b148bd166402194
Gilly <67492466+Gilly312@users.noreply.github.com>
2022-05-19 16:27:02 +0700

    asd

--- Products/Product.js
+++ Products/Product.js
@@ -17,11 +17,36 @@ const Product = mongoose.Schema({
         type: String,
         required: true
     },
+
     category: {
         type: String,
         required: true
     },
-    basedGame: {
+    size: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    createdAt: {   
+        type: Date,
+        default: Date.now
+    },
+    sale: {
+        type: Boolean,
+        default: false
+    },
+    type: {
         type: String,
         required: true
     }
--- Products/products.js
+++ Products/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
 
 const Product = require("./Product");
@@ -7,47 +7,48 @@ const Product = require("./Product");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/products/search', async (req, res) => {
-    const products = await Product.find({
-        name: {
-            $regex: req.body.name,
-            $options: 'i'
-        }
-    });
-    return res.json(products)
-})
 app.get("/products", async (req, res) => {
-  await Product.find()
-    .then((products) => {
-      return res.json(products);
-    })
-    .catch((err) => {
-      return res.json(err);
-    });
+  const products = await Product.find();
+  return res.json(products);
 });
+
+app.get("/products/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  return res.json(product);
+});
+
+app.get("/products/search", async (req, res) => {
+  const input = req.body.input;
+  const products = await Product.find({ name: input });
+  return res.json(products);
+});
+
 app.post("/products/add", async (req, res) => {
-  await Product.create(req.body).then((product) => {
-    return res.json(product);
+  const product = new Product(req.body);
+  await product.save();
+  return res.json({
+    status: "Product saved",
   });
 });
-app.put("/products/update/:id", async (req, res) => {
-  await Product.findByIdAndUpdate(req.params.id, req.body)
-    .then((product) => {
-      return res.json(product);
-    })
-    .catch((err) => {
-      return res.json(err);
-    });
+
+app.put("/products/:id", async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  return res.json(product);
+  // const product = await Product.findById(req.params.id);
+  // product.name = req.body.name;
+  // product.price = req.body.price;
+  // product.description = req.body.description;
+  // product.image = req.body.image;
 });
-app.delete("/products/delete/:id", async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id)
-    .then((product) => {
-      return res.json(product);
-    })
-    .catch((err) => {
-      return res.json(err);
-    });
-})
+
+app.delete("/products/:id", async (req, res) => {
+  await Product.findByIdAndRemove(req.params.id);
+  return res.json({
+    status: "Product deleted",
+  });
+});
 
 app.listen(3000, async () => {
   mongoose.connect("mongodb://127.0.0.1:27017/new").then(() => {
