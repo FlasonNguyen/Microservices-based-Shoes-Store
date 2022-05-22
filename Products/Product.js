9a816a034e350cec76ea817e0caf51b8bd1ceada
FlasonNguyen <30571019+FlasonNguyen@users.noreply.github.com>
2022-05-22 15:19:20 +0700

    Update

--- Products/Product.js
+++ Products/Product.js
@@ -1,55 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Product = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-
-    category: {
-        type: String,
-        required: true
-    },
-    size: {
-        type: String,
-        required: true
-    },
-    color: {
-        type: String,
-        required: true
-    },
-    brand: {
-        type: String,
-        required: true
-    },
-    quantity: {
-        type: Number,
-        required: true
-    },
-    createdAt: {   
-        type: Date,
-        default: Date.now
-    },
-    sale: {
-        type: Boolean,
-        default: false
-    },
-    type: {
-        type: String,
-        required: true
-    }
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  sizes: {
+    type: Array,
+    required: true,
+  },
+  sale: {
+    type: Boolean,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  genders: {
+    type: String,
+  },
 });
-
-module.exports = mongoose.model('Product', Product);
+module.exports = mongoose.model("Product", ProductSchema);
--- Products/products.js
+++ Products/products.js
@@ -3,55 +3,39 @@ const mongoose = require("mongoose");
 const app = express();
 
 const Product = require("./Product");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", async (req, res) => {
-  const products = await Product.find();
-  return res.json(products);
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
-
-app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  return res.json(product);
+app.get("/api/products/details", async (req, res) => {
+  try {
+    const product = await Product.findById(req.body.id);
+    res.json(product);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
+app.post('/api/products/add', async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+})
 
-app.get("/products/search", async (req, res) => {
-  const input = req.body.input;
-  const products = await Product.find({ name: input });
-  return res.json(products);
-});
-
-app.post("/products/add", async (req, res) => {
-  const product = new Product(req.body);
-  await product.save();
-  return res.json({
-    status: "Product saved",
-  });
-});
-
-app.put("/products/:id", async (req, res) => {
-  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  return res.json(product);
-  // const product = await Product.findById(req.params.id);
-  // product.name = req.body.name;
-  // product.price = req.body.price;
-  // product.description = req.body.description;
-  // product.image = req.body.image;
-});
-
-app.delete("/products/:id", async (req, res) => {
-  await Product.findByIdAndRemove(req.params.id);
-  return res.json({
-    status: "Product deleted",
-  });
-});
-
-app.listen(3000, async () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/new").then(() => {
-    console.log("http://localhost:3000/");
+app.listen(process.env.PORT, async () => {
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log(`"http://localhost:${process.env.PORT}/"`);
   });
 });
