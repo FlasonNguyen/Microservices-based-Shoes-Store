38a56995122a1c3c429640b9259858c13b724406
FlasonNguyen <30571019+FlasonNguyen@users.noreply.github.com>
2022-05-25 09:37:49 +0700

    Commit

--- Products/products.js
+++ Products/products.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const Product = require("./Product");
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
